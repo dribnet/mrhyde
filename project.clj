074bf22cda540b47c47ecbf051a50dd8324a3cd3
Tom White <tom@sixdozen.com>
2015-11-17 09:27:27 -0800

    release: 0.5.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.drib/mrhyde "0.5.4-SNAPSHOT"
+(defproject net.drib/mrhyde "0.5.5"
   :description "mrhyde: cljs <-> js interop"
   :url "https://github.com/dribnet/mrhyde"
   :license {:name "Eclipse Public License"
